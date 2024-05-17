f24710f539a85e28159bba6409029fa577f63fda
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-17 16:57:37 +0300

    add Lesson_3:Task_2

---
+++ Lesson_3/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintSqaures(int limit)
+{
+    int i = 1;
+    while (i < limit)
+    {
+        System.Console.Write($"{i*i}, ");
+        i++;
+    }
+    System.Console.WriteLine(limit*limit);
+}
+int b = Convert.ToInt32(Console.ReadLine());
+PrintSqaures(b);
