90f5eb3d5a52523bf984b9d94e32c25d947545cf
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-17 15:56:56 +0300

    add Lesson_3:Task_1

---
+++ Lesson_3/Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denuminator = c + d;
+    double result = numinator / denuminator;
+    return result;
+}
+
+System.Console.WriteLine(CalculateFormula(2, 2, 1, 3));
