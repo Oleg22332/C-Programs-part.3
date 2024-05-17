39c879bd8d2264309fd76dff45de29ac2e2e543f
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-17 13:15:43 +0300

    add Lesson_2:Task_3

---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = 10;
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+for (int i = 0; i < n; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
