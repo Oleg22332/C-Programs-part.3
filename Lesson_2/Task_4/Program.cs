6241f607338339a5203221ec87d388fc960c01ec
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-17 13:16:56 +0300

    add Lesson_2:Task_4

---
+++ Lesson_2/Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 5;
+int[] array = {3, 4, 7, 4, 23};
+int max = array[1];
+for (int i = 0; i < n; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine(max);
