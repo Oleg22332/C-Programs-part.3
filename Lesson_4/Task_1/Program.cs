c2444043aef0843bbd20412db410a565fcc0a1de
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-18 18:50:56 +0300

    Практическое задание к уроку 6, задача 1

---
+++ Lesson_4/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿char[,] matrix = {
+    {'с', 'т', 'р'},
+    {'о', 'к', 'а'}
+};
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        System.Console.Write(matrix[i,j]);
+    }
+}
