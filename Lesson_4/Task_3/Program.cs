b0763098f300905bbc99686795c7c972b52204e3
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-18 19:53:50 +0300

    Практическое задание к уроку 6, задача 3

---
+++ Lesson_4/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿string originalString = "потоп";
+        char[] charArray = originalString.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedString = new string(charArray);
+if (originalString == reversedString)
+{
+    System.Console.WriteLine("является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("не является палиндромом");
+}T
