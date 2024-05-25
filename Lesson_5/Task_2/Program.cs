2e8728f1bf0fb5aa22ed49151cd0880b2f4bf670
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-25 23:02:58 +0300

    Практическое задание к уроку 7, задача 2

---
+++ Lesson_5/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int m = 2;
+int n = 1;
+int result = AckermanFunction(m, n);
+static int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return AckermanFunction(m - 1, 1);
+    else
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+Console.WriteLine($"Ackermann({m}, {n}) = {result}");
