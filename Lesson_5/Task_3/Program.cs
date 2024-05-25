dac601d515a48fd14c392579b2a3e6e414c35382
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-25 22:53:51 +0300

    Практическое задание к уроку 7, задача 3

---
+++ Lesson_5/Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = {1, 5, 5, 10, 8, 9};
+void PrintArrayReverse(int[] arr, int i)
+{
+    if (i < 0)
+    {
+        return;
+    }
+    Console.WriteLine(arr[i]);
+    PrintArrayReverse(arr, i - 1);
+}
+PrintArrayReverse(array, array.Length - 1);
