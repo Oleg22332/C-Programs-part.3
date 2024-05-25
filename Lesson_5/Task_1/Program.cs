9ef0a473ccc8ed4eab8ceb4b4487f172b5de0199
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-05-25 20:47:15 +0300

    Практическое задание к уроку 7, задача 1

---
+++ Lesson_5/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int Enumeration(int a, int b)
+{
+    if (a == b)
+    {
+        return b;
+    }
+    System.Console.WriteLine(a);
+    return Enumeration(a + 1, b);
+}
+System.Console.WriteLine(Enumeration(1, 10));
